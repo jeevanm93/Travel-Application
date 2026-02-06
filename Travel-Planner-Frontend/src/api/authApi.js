84191ce8380b81fa7c13366400dea7424ea29748
Jeevan M <130200754+jeevanm93@users.noreply.github.com>
2026-02-06 17:18:04 +0530

    Delete Travel-Planner-Frontend directory

--- Travel-Planner-Frontend/src/api/authApi.js
+++
@@ -1,9 +0,0 @@
-import axiosClient from "./axiosClient";
-
-export const registerUser = (data) => {
-  return axiosClient.post("/auth/register", data);
-};
-
-export const loginUser = (data) => {
-  return axiosClient.post("/auth/login", data);
-};
